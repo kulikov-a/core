380dc9450800c98265a9da258526b10586ab122a
kulikov-a <36099472+kulikov-a@users.noreply.github.com>
2021-04-05 11:49:52 +0300

    Update LvtemplatesController.php

--- src/opnsense/mvc/app/controllers/OPNsense/Diagnostics/Api/LvtemplatesController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Diagnostics/Api/LvtemplatesController.php
@@ -1,7 +1,9 @@
 <?php
 
 /**
+ *    Copyright (C) 2021 Deciso B.V.
  *
+ *    All rights reserved.
  *    Redistribution and use in source and binary forms, with or without
  *    modification, are permitted provided that the following conditions are met:
  *
